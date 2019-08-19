ddd4ff5204496ee32569aad906bc640318e322c3
Weigo <wesstation2@gmail.com>
2019-08-20 00:38:27 +0800

    類型

---
+++ 1.basement/6.types.py
@@ -0,0 +1,14 @@
+a = 123
+b = "文字"
+c = "abc"
+d = False
+
+# type() 檢查類型
+
+print(type(a)) #int == 數字
+print(type(b)) #str == 字串
+print(type(c)) 
+print(type(d)) #bool == 布林值(yes or no)
+
+# 兩個等於是判斷的意思，判斷b,c 是不是兩種不一樣的type
+print(type(b) == type(c))
